a0e700c6269c972496a4bf2e7c75ee8027d96aa1
rob kurtz <rob@robkurtz.net>
2017-01-11 17:21:31 -0700

    play with config

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.static('build'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  const appPath = path.join(__dirname, 'build/index.html');
+  console.log('SERVING FROM', appPath)
+  res.sendFile(appPath);
 });
 
 app.listen(process.env.PORT || 3050, () => console.log('Listening on port ' + process.env.PORT));
