d89a7bcaf8f9d367ecfa3b0fbdb3fd432aec2af0
rob kurtz <rob@robkurtz.net>
2017-01-11 17:17:54 -0700

    3rd times a charm

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+
+const app = express();
 
 app.use(express.static('build'));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
-app.listen(process.env.PORT || 8080);
-console.log('listening')
+app.listen(process.env.PORT || 3050, () => console.log('Listening on port ' + process.env.PORT));
