436d4144782f825958686f2b9e631432c6d07267
rob kurtz <rob@robkurtz.net>
2017-01-11 17:01:52 -0700

    expermienting with config

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('build'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build/index.html'));
+// });
 
 app.listen(process.env.PORT || 8080);
